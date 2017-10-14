7f55df3b68267261cc2d5883a679af10689bf61b
Ryan Holstein <Ryan.c.Holstein@biola.edu>
2017-10-13 20:23:26 -0700

    new search

--- main/apps/homeApp/static/global/js/nav-bar.js
+++ main/apps/homeApp/static/global/js/nav-bar.js
@@ -69,33 +69,24 @@ $(document).ready(function(){
 
 // functions for top nav open/close =============
 function openSearch() {
-  searchAppear();
+  $('.search-div').css({display: 'inline-block'})
 
 };
 
 function closeSearch(){
-  searchAppear();
+  $('.search-div').css({display: 'none'})
   $('.hamburger').animate({opacity: '1'});
   $('.icon-cancel').css({'fill': '#222222'});
   $('.icon-cancel').css({display: 'none'});
   $('.menu-button-main').animate({"background-color": "white"});
 };
 
-function searchAppear() {
-  var hidden = $('.search-wrapper');
-  if (hidden.hasClass('visible')){
-    hidden.animate({"right":"-1000px"}, "slow").removeClass('visible');
-  } else {
-    hidden.animate({"right":"50px"}, "slow").addClass('visible');
-  }
-
-}
 
 function jsonTaken(json){
   $('#search-results').html('')
   console.log(json);
   if (json.length == 0) {
-    $("#search-results").append('<h2>No Results Found</h2>');
+    $("#search-results").append('<h2 class = "no-results-found" >No Results Found</h2>');
   }
   else {
   for (var i = 0; i < json.length; i++){
@@ -105,14 +96,14 @@ function jsonTaken(json){
     console.log(id);
     console.log(img_url);
     if (type == 'movie'){
-      $("#search-results").append('<a href="/movie/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <h3 class="search-result-title">' + json[i].name + '<h3></div></a>');
+      $("#search-results").append('<a href="/movie/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <div class="title-holder"><h3 class="search-result-title">' + json[i].name + '</h3></div> <h3 class="movie-type type">Movie</h3> </div></a>');
     }
     else if (type == 'tv') {
-      $("#search-results").append('<a href="/show/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <h3 class="search-result-title">' + json[i].name + '<h3></div></a>');
+      $("#search-results").append('<a href="/show/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <div class="title-holder"><h3 class="search-result-title">' + json[i].name + '</h3></div><h3 class="tv-type type">Television</h3> </div></a>');
 
     }
     else if (type =='person') {
-      $("#search-results").append('<a href="/people/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <h3 class="search-result-title">' + json[i].name + '<h3></div></a>');
+      $("#search-results").append('<a href="/people/' + id + '"><div class="result-search center"><img class ="search-result-icon" src="'+ img_url +'""> <div class="title-holder"><h3 class="search-result-title">' + json[i].name + '</h3></div><h3 class="person-type type">Person</h3></div></a>');
     }
 
   }
